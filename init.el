676b129493bcbcd94065b8df2a45758434b01721
Abhishek Amralkar <abhishekamralkar@gmail.com>
2021-03-03 23:39:26 +0530

    adding the Emacs org file

--- init.el
+++ init.el
@@ -1,140 +1,103 @@
-(setq inhibit-startup-message t)
+;; NOTE: init.el is now generated from Emacs.org.  Please edit that file
+;;       in Emacs and init.el will be generated automatically!
 
-(scroll-bar-mode -1) ; Disable visible scrollbar
-(tool-bar-mode -1)   ; Disable the toolbar
-(tooltip-mode -1)    ; Disable tooltips
-(set-fringe-mode 10) ; 
+;; You will most likely need to adjust this font size for your system!
+(defvar aaa/default-font-size 100)
+(defvar aaa/default-variable-font-size 100)
 
-(menu-bar-mode -1)   ; Disable the menu bar
-
-(setq visible-bell t); Set up the visible bell
-
-;; Set Fonts
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 100)
-
-(load-theme 'wombat) ; Load Theme
+;; Make frame transparency overridable
+(defvar aaa/frame-transparency '(90 . 90))
 
 ;; Initialize package sources
-
 (require 'package)
 
-(setq package-archives '(("melpa". "https://melpa.org/packages/")
-			 ("org"  . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Initialize use-package
-
+  ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
+;; NOTE: If you want to move everything out of the ~/.emacs.d folder
+;; reliably, set `user-emacs-directory` before loading no-littering!
+;(setq user-emacs-directory "~/.cache/emacs")
+
+(use-package no-littering)
+
+;; no-littering doesn't set this by default so we must place
+;; auto save files in the same path as it uses for sessions
+(setq auto-save-file-name-transforms
+      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)            ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
-;; Disable line numbers for some mode
+;; Set frame transparency
+(set-frame-parameter (selected-frame) 'alpha aaa/frame-transparency)
+(add-to-list 'default-frame-alist `(alpha . ,aaa/frame-transparency))
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
-		term-mode-hook
-		eshell-mode-hook))
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(use-package command-log-mode)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height aaa/default-font-size)
+
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height aaa/default-font-size)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height aaa/default-variable-font-size :weight 'regular)
+
+;; Automatically tangle our Emacs.org config file when we save it
+(defun aaa/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/Code/my-emacs/Emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'aaa/org-babel-tangle-config)))
+
+(use-package doom-themes
+  :init (load-theme 'doom-palenight t))
 
 (use-package all-the-icons)
 
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line) 
-         ("C-k" . ivy-previuos-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previos-line)
-         ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previos-line)
-	 ("C-d" . ivy-reverse-i-search-kill))        
-  :config
-  (ivy-mode 1))
-
-
-;; Dooms Mode Line
 (use-package doom-modeline
-  :ensure t
   :init (doom-modeline-mode 1)
-  :custom ((dooms-modeline-height 8)))
-
-(use-package doom-themes
-  :init (load-theme 'doom-dracula t))
-
-;; Rainbow Delimiters
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-;; which-key
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-dely 0.1))
-
-;; ivy rich
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
-
-;; counsel
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history)))
-
-;; helpful
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-
-(use-package hydra)
-
-(defhydra hydra-text-scale (:timeout 6)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
-
-;;(use-package exwm)
-;;(require 'exwm)
-;;(require 'exwm-config)
-;;(exwm-config-example)
-
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  (when (file-directory-p "~/Code")
-    (setq projectile-project-search-path '("~/Code")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package magit
-  :commands (magit-status magit-get-current-branch)
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+  :custom ((doom-modeline-height 15)))
